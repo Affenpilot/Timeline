d87a6543af58c522556b71c16b2c419241d207a6
Florian Büchner <flori.buechner90@googlemail.com>
2017-09-17 23:37:30 +0200

    Create Controllers

--- src/Controllers/PostController.php
+++ src/Controllers/PostController.php
@@ -3,8 +3,52 @@
 namespace Affenpilot\Timeline\Controllers;
 
 use App\Http\Controllers\Controller;
+use Affenpilot\Timeline\Models\Post;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //do something
+    /**
+     * @param Request $request
+     * @param $user_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function postCreatePost(Request $request, $user_id)
+    {
+        $this->validate($request, [
+            'body' => 'required|max:1000'
+        ]);
+
+        $request->user()->createPost(
+            $request['body'],
+            $request->user(),
+            $request->user()->timelinefrom($request['_class'], 'id')->first()
+        );
+
+        return redirect()->route(
+            'timeline',
+            ['user_id' => $user_id]
+        )->with(['message' => 'Post was successfully saved.']);
+    }
+
+    /**
+     * @param $post_id
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function getDeletePost($post_id)
+    {
+        $post = Post::where('id', $post_id)->first();
+
+        if (Auth::user() != $post->user) {
+            return redirect()->back();
+        }
+
+        $post->delete();
+        return redirect()->route(
+            'timeline',
+            ['user_id' => Auth::user()->id]
+        )->with(['message' => 'Successfully deleted.']);
+    }
 }
