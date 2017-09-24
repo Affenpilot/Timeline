f680e655f403bc3039ca29b3c141c2ab2295187b
Florian Büchner <flori.buechner90@googlemail.com>
2017-09-24 21:54:16 +0200

    change table name

--- database/migrations/2016_12_13_130626_create_timeline_posts_table.php
+++ database/migrations/2016_12_13_130626_create_timeline_posts_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreatePostsTable extends Migration
+class CreateTimelinePostsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreatePostsTable extends Migration
      */
     public function up()
     {
-        Schema::create('posts', function (Blueprint $table) {
+        Schema::create('timeline_posts', function (Blueprint $table) {
             $table->increments('id');
             $table->text('body');
             $table->morphs('author');
@@ -29,6 +29,6 @@ class CreatePostsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('posts');
+        Schema::dropIfExists('timeline_posts');
     }
 }
